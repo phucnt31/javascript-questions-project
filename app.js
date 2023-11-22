d7428ae5c8609d57b5ac890074f72df9382c1a73
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-22 17:13:06 +0700

    Using selectors inside the element to toggle quesion and handle closing question when opening another one

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
 
-// traversing the dom
-const btns = document.querySelectorAll(".question-btn");
-
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement;
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+        console.log(item);
+      }
+    });
     question.classList.toggle("show-text");
   });
 });
+
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
